aeb5d2bbc6bff8aeeb44efde3a1778805c35d9e3
travisneilson <travisneilson@gmail.com>
2013-11-08 13:54:37 -0800

    added a few images of the poster

--- assets/post-assets/044-enders-game-poster/js/functions.js
+++ assets/post-assets/044-enders-game-poster/js/functions.js
@@ -1,39 +1,17 @@
-(function( $ ){
-
-  $.fn.fitText = function( kompressor, options ) {
-
-    // Setup options
-    var compressor = kompressor || 1,
-        settings = $.extend({
-          'minFontSize' : Number.NEGATIVE_INFINITY,
-          'maxFontSize' : Number.POSITIVE_INFINITY
-        }, options);
-
-    return this.each(function(){
-
-      // Store the object
-      var $this = $(this);
-
-      // Resizer() resizes items based on the object width divided by the compressor * 10
-      var resizer = function () {
-        $this.css('font-size', Math.max(Math.min($this.width() / (compressor*10), parseFloat(settings.maxFontSize)), parseFloat(settings.minFontSize)));
-      };
-
-      // Call once to set.
-      resizer();
-
-      // Call on resize. Opera debounces their resize by default.
-      $(window).on('resize.fittext orientationchange.fittext', resizer);
-
-    });
-
-  };
-
-})( jQuery );
-
 $(function() {
 
-  jQuery(".enders-game-poster h1").fitText();
-
+  $('.e-thumb').click(function() {
+    var $this = $(this),
+        imgUrl = $this.css('background-image'),
+        $template = '<div class="overlay" style="background-image:'+imgUrl+'"></div>';
+    
+    $('body').append($template);
+    
+  });
+  
+  $('body').on('click', '.overlay', function() {
+    $(this).remove();
+  });
+  
 });
 
