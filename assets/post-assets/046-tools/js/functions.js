080df4743edffdaac16eba1c0434f2a024f78ed5
Travis Neilson <travisneilson@gmail.con>
2014-05-27 12:25:03 -0700

    tools progress

--- assets/post-assets/046-tools/js/functions.js
+++ assets/post-assets/046-tools/js/functions.js
@@ -1,3 +1,20 @@
 $(function() {
 
 });
+
+
+$(window).scroll(function() {
+
+    slidingHead();
+    
+});
+
+
+function slidingHead() {
+  windowScroll = $(this).scrollTop();
+  $('.bunny > div').css({
+    'top' : "-" + windowScroll + "px",
+    'left' : "-" + windowScroll + "px",
+    'right' : "-" + windowScroll + "px"
+  });
+}
