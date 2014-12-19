b12c34d90854fa0b10eaf4fec53a45d3d8a733db
travisneilson <travisneilson@gmail.com>
2014-12-19 13:06:52 -0800

    return of no clicky

--- assets/post-assets/047-poems/js/functions.js
+++ assets/post-assets/047-poems/js/functions.js
@@ -1,24 +1,12 @@
 $(function() {
   
+  noClicky();
+  
+});
+
+
+function noClicky() {
   $('.poem-target').click(function() {
     return false;
   });
-  
-});
-
-
-$(window).scroll(function() {
-
-  var windowScroll = $(this).scrollTop();
-  
-  $('.bunny > div').css({
-    'top' : "-" + windowScroll + "px",
-    'left' : "-" + windowScroll + "px",
-    'right' : "-" + windowScroll + "px"
-  });
-    
-});
-
-
-
-  
+}
